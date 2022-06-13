c0af9a3816e67cd42b6b76c64791fe490a6c77b7
Dax2976 <98029197+Dax2976@users.noreply.github.com>
2022-06-13 15:27:46 +0200

    initial and final commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{('css/app.css')}}">
+
+    </head>
+    <header>
+        <h1 class="text-center text-succes py-4">Films</h1>
+    </header>
+    <body class="bg-dark">
+        <div class="container">
+            <div class="row p-4 justify-content-center">
+                @foreach ($movies as $key => $movie)
+                <div class="card col-3 m-2" style="">
+                    <img class="card-img-top w-50 align-self-center" src="https://img.wallpapic.it/i0389-833-517/medium/halloween-film-notte-buio-sfondo.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Titolo: {{$movie->title}}</h5>
+                      <h6 class="card-title mb-5">Titolo Originale: {{$movie->original_title}}</h6>
+                      <div class="card-text">
+                        <p>Nazionalità: {{$movie->nationality}}</p>
+                        <p>Data di Uscita: {{$movie->date}}</p>
+                        <p>Voto: {{$movie->vote}}</p>
+                      </div>
+                    </div>
+                  </div>
+                @endforeach
+            </div>
+        </div>
+
+    </body>
+</html>
